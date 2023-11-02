092737ec0c8fb0139e2059b1b8a11fed41f19f8f
Rise Erpelding <rise@heysparkbox.com>
2023-11-02 06:31:41 -1000

    feat: wire up copy button

--- src/CopyButton.tsx
+++ src/CopyButton.tsx
@@ -1,6 +1,17 @@
+import { generatedPassword } from "./App";
+
 export default function CopyButton() {
+
+  const handleCopyClick = async () => {
+    try {
+      await navigator.clipboard.writeText(generatedPassword());
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+    }
+  }
+  
   return (
-    <button type="button" class="password-section__copy-button">
+    <button type="button" class="password-section__copy-button" onclick={handleCopyClick}>
       <span class="password-section__copy-button-icon-container">
         <svg viewBox="0 0 24 24">
           <path d="M16 1H4C3 1 2 2 2 3v14h2V3h12V1zm3 4H8C7 5 6 6 6 7v14c0 1 1 2 2 2h11c1 0 2-1 2-2V7c0-1-1-2-2-2zm0 16H8V7h11v14z"></path>
