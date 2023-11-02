47f539e2dbc4f433b22fc6b5fe90a05ac5eaf6f3
Rise Erpelding <rise@heysparkbox.com>
2023-11-02 10:57:10 -1000

    refactor: move App.scss styles to ITCSS-ified style dir

--- src/SvgX.tsx
+++ src/SvgX.tsx
@@ -1,6 +1,6 @@
 export default function SvgX() {
   return (
-    <div class="password-art__x">
+    <div class="password-graphic__x">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
         <line x1="5" x2="25" y1="5" y2="25" stroke="black" stroke-width="5" stroke-linecap="round" />
         <line x1="25" x2="5" y1="5" y2="25" stroke="black" stroke-width="5" stroke-linecap="round" />
