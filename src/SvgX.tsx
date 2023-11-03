5a7f67305e67fbc43b706fe1d8c170a3f0851cd1
Rise Erpelding <rise@heysparkbox.com>
2023-11-03 12:40:03 -1000

    feat: x animations

--- src/SvgX.tsx
+++ src/SvgX.tsx
@@ -2,8 +2,8 @@ export default function SvgX() {
   return (
     <div class="password-graphic__x">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
-        <line x1="5" x2="25" y1="5" y2="25" stroke="black" stroke-width="5" stroke-linecap="round" />
-        <line x1="25" x2="5" y1="5" y2="25" stroke="black" stroke-width="5" stroke-linecap="round" />
+        <line x1="5" x2="25" y1="5" y2="25" stroke-width="5" stroke-linecap="round" />
+        <line x1="25" x2="5" y1="5" y2="25" stroke-width="5" stroke-linecap="round" />
       </svg>
     </div>
   )
